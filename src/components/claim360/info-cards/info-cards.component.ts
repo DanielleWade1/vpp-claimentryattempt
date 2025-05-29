d87dbac257280e662258e81a37033a71c9ac3d02
DanielleWade1 <danielle.wade@skygenusa.com>
2025-05-29 14:08:21 -0500

    Move Place of Service field to provider info card

--- src/components/claim360/info-cards/info-cards.component.ts
+++ src/components/claim360/info-cards/info-cards.component.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-info-cards',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [CommonModule, MatCardModule, MatButtonModule, MatSelectModule, FormsModule],
   template: `
     <div class="section-header">
       <h2>Selected Information</h2>
@@ -28,6 +30,14 @@ import { MatButtonModule } from '@angular/material/button';
           <p>{{providerInfo.practice}}</p>
           <p>{{providerInfo.address}}</p>
           <p>{{providerInfo.city}}, {{providerInfo.state}} {{providerInfo.zip}}</p>
+          <mat-form-field appearance="outline" class="place-of-service">
+            <mat-label>Place of Service</mat-label>
+            <mat-select [(ngModel)]="selectedPlaceOfService">
+              <mat-option *ngFor="let option of placeOfServiceOptions" [value]="option.code">
+                {{option.name}}
+              </mat-option>
+            </mat-select>
+          </mat-form-field>
         </mat-card-content>
       </mat-card>
     </div>
@@ -78,6 +88,11 @@ import { MatButtonModule } from '@angular/material/button';
       color: #666;
     }
 
+    .place-of-service {
+      width: 100%;
+      margin-top: 16px;
+    }
+
     @media (max-width: 1024px) {
       .info-tiles {
         grid-template-columns: 1fr;
@@ -88,6 +103,20 @@ import { MatButtonModule } from '@angular/material/button';
 export class InfoCardsComponent {
   @Input() patientInfo: any;
   @Input() providerInfo: any;
+
+  selectedPlaceOfService: string = '';
+  placeOfServiceOptions = [
+    { code: '11', name: 'Office' },
+    { code: '12', name: 'Home' },
+    { code: '19', name: 'Off Campus-Outpatient Hospital' },
+    { code: '22', name: 'On Campus-Outpatient Hospital' },
+    { code: '23', name: 'Emergency Room - Hospital' },
+    { code: '24', name: 'Ambulatory Surgical Center' },
+    { code: '31', name: 'Skilled Nursing Facility' },
+    { code: '32', name: 'Nursing Facility' },
+    { code: '33', name: 'Custodial Care Facility' },
+    { code: '34', name: 'Hospice' }
+  ];
 
   onUpdate() {
     // Emit event to parent
